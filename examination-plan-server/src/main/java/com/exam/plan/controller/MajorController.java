8d71aae885249c6d023915c18e956668f7d8d49b
Janspiry <1596343047@qq.com>
2020-06-28 19:48:37 +0800

    专业大类修改bug的更正，考次管理搜索成功的颜色变更

--- examination-plan-server/src/main/java/com/exam/plan/controller/MajorController.java
+++ examination-plan-server/src/main/java/com/exam/plan/controller/MajorController.java
@@ -156,13 +156,7 @@ public class MajorController {
         return ResultGenerator.genOkResult(pageInfo);
     }
 
-    @PostMapping("/category")
-    @ResponseBody
-    public Result categoryAdd(@RequestBody final MajorCategory major) {
-        System.out.println("addMajor major=" + major);
-        majorCategoryService.save(major);
-        return ResultGenerator.genOkResult();
-    }
+
     @DeleteMapping("/category/{id}")
     @ResponseBody
     public Result categoryDelete(@PathVariable final String id, final Principal principal) {
@@ -179,11 +173,16 @@ public class MajorController {
     @ResponseBody
     public Result categoryUpdate(
             @PathVariable final String id, @RequestBody final MajorCategory major, final Principal principal) {
-        final MajorCategory dbMajor = this.majorCategoryService.getById(id);
-        if (dbMajor == null) {
-            return ResultGenerator.genFailedResult("选择的数据不存在，请检查");
-        }
+        System.out.println("updateMajorCategory id=" + id);
         this.majorCategoryService.update(major);
+        return ResultGenerator.genOkResult();
+    }
+
+    @PostMapping("/category")
+    @ResponseBody
+    public Result categoryAdd(@RequestBody final MajorCategory major) {
+        System.out.println("addMajor major=" + major);
+        majorCategoryService.save(major);
         return ResultGenerator.genOkResult();
     }
 
